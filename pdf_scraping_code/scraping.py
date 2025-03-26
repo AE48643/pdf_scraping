a880ea503e4cd92109f55e015e74257bc977697d
AE48643 <122492547+AE48643@users.noreply.github.com>
2025-03-26 20:10:11 +0000

    accessing_Img_to_text

--- pdf_scraping_code/scraping.py
+++ pdf_scraping_code/scraping.py
@@ -1,6 +1,6 @@
 import pdfplumber
 
-pdf_file_path = "/workspaces/pdf_scraping/pdf_file/Degree Audit.pdf"
+pdf_file_path = "/workspaces/pdf_scraping/pdf_file/DA SP25.pdf"
 
 with pdfplumber.open(pdf_file_path) as pdf:
     for page in pdf.pages:
